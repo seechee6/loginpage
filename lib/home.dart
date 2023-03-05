5e005e58d42693a665999b228344520c8bdf4280
seechee6 <seechee06@gmail.com>
2023-03-05 22:38:53 +0800

    Added my project

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("My Home Page"),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(color: Colors.lightGreen),
+          child: const Center(
+            child: Text("Welcome to home page ",
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30)),
+          ),
+        ));
+  }
+}
